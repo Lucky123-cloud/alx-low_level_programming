d7e44b25f84a9c94cf4bf5f227994cb4b6f5f31e
Lucky <luckybaraka21@gmail.com>
2023-10-11 11:42:22 -0700

    tough one

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - this is the main function of the task
+ * Return: returns 0
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar (c);
+	}
+
+	_putchar('\n');
+
+}
