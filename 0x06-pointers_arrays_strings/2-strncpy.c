3f373407d2584b580f4d9464381d43dfedf8f450
Lucky <luckybaraka21@gmail.com>
2023-10-18 22:12:18 -0700

    i am in the third question

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strncpy - The main function of the code
+  *
+  * @src: Function parameter
+  *
+  * @dest: Function parameter
+  *
+  * @n: Function parameter
+  *
+  * Return: dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
