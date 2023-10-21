466657b416f010cd8f4d6060425dee83ee1344cd
Lucky <luckybaraka21@gmail.com>
2023-10-20 21:50:35 -0700

    I am a hero

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <unistd.h>
 #include <stdio.h>
 
-void _puts(char *str)
-int _putchar(char c)
+void _puts(char *str);
+int _putchar(char c);
 void _puts(char *str);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
