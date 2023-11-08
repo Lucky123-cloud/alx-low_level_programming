f371af05b10bf26c7b0068d087d0eac858f9372c
Lucky <luckybaraka21@gmail.com>
2023-11-08 07:57:44 -0800

    I love my work but somtimes it gets tough

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,9 @@
 	 * print_name - print name using pointer to function
 	 * @name: string to add
 	 * @f: pointer to function
-	 * Description: This function takes a string and function as parameters
-	 * It checks for NULL pointers and then calls the provided function
 	 * Return: nothing
-	 **/
+	 */
+
 	void print_name(char *name, void (*f)(char *))
 	{
 		if (name == NULL || f == NULL)
